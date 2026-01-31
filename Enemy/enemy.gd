07c5b03eea797d01dad05c6c8b7cd325ffc1be9f
skyking336 <3336jason@gmail.com>
2026-01-31 16:38:29 +0800

    Wave Spawner

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -11,16 +11,15 @@ extends CharacterBody2D
 var attacked = false
 var player_in_range = false
 
-
 func _physics_process(_delta: float) -> void:
 	var player_direction = (player.global_position - global_position).normalized()
 	velocity =  player_direction * SPEED
 	move_and_slide()
 	if player_direction.x < 0:
-		$SlimeAnimation.flip_h = true
+		$EnemyAnimation.flip_h = true
 		$AttackRange/AttackCollision.position = -abs($AttackRange/AttackCollision.position)
 	else:
-		$SlimeAnimation.flip_h = false
+		$EnemyAnimation.flip_h = false
 		$AttackRange/AttackCollision.position = abs($AttackRange/AttackCollision.position)
 	if attacked == false and player_in_range == true:
 		player.got_damaged(basic_damage)
@@ -32,10 +31,10 @@ func got_damaged():
 	health -= 1
 	if health <= 0:
 		queue_free()
-	$SlimeAnimation.stop()
-	$SlimeAnimation.play("hurt")
-	await $SlimeAnimation.animation_finished
-	$SlimeAnimation.play("idle")
+	$EnemyAnimation.stop()
+	$EnemyAnimation.play("hurt")
+	await $EnemyAnimation.animation_finished
+	$EnemyAnimation.play("idle")
 
 
 func _on_attack_range_body_entered(body: Node2D) -> void:
