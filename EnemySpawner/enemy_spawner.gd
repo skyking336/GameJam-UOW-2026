5cc761bc10fb12dcc495109d0a5b980773beb404
k3mystra <programmer.coleslaw981@slmail.me>
2026-01-31 12:13:31 +0800

    feat: Use array of enemies

--- EnemySpawner/enemy_spawner.gd
+++ EnemySpawner/enemy_spawner.gd
@@ -3,21 +3,19 @@ extends Node2D
 @export var player_group = "DummyPlayer"
 @export var player_object_name = "Player"
 
-@export var Enemy1 : PackedScene
-@export var Enemy2 : PackedScene
+@export var enemies : Array[PackedScene]
 
 @onready var player = get_tree().get_first_node_in_group(player_group)
 
 func _ready():
 	$MobSpawnCooldown.start()
-	
-func _process(delta):
+
+func _process(_delta):
 	global_position = player.global_position
 
 func _on_mob_spawn_cooldown_timeout() -> void:
-	var enemy = Enemy1.instantiate()
+	var enemy = enemies[0].instantiate()
 	$Path2D/MobSpawnArea.progress_ratio = randf()
-	$EnemySpawnLocation.position = $Path2D/MobSpawnArea.position
 	enemy.position = $Path2D/MobSpawnArea.position
 	get_parent().add_child(enemy)
 	print(str($Path2D/MobSpawnArea.position))
