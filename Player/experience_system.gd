d0f0ff5df60eb465298ea4a495acd2b1fe712e26
skyking336 <3336jason@gmail.com>
2026-01-31 20:28:12 +0800

    TimePausing

--- Player/experience_system.gd
+++ Player/experience_system.gd
@@ -3,6 +3,8 @@ extends Node
 var current_level = 1
 var level_up_threshold = 200
 
+var phase_1_got = false
+
 signal signal_invincible
 
 func check_level_up(exp : int):
@@ -14,9 +16,13 @@ func check_level_up(exp : int):
 	
 	if current_level == 5:
 		signal_invincible.emit()
+		if phase_1_got == false:
+			var tree = get_tree().get_first_node_in_group("MainScene")
+			tree.pause_scene()
+			
+		phase_1_got = true
 		print("phase 1 completed")
 		#here need to make the logic for converting enemy into player
-		
 		
 	if current_level == 10:
 		signal_invincible.emit()
--- main_scene.gd
+++ main_scene.gd
@@ -4,4 +4,8 @@ var pause_times
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().paused = not get_tree().paused
+		pause_scene()
+	
+
+func pause_scene():
+	get_tree().paused = not get_tree().paused
