eda98ce151110810649240194b5e8b4585587706
k3mystra <programmer.coleslaw981@slmail.me>
2026-02-01 09:13:10 +0800

    feat: Add MainMenu

---
+++ MainMenu/main_menu.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+@export var main_scene: PackedScene
+
+func _on_start_button_pressed() -> void:
+	print("Start button pressed")
+	get_tree().change_scene_to_packed(main_scene)
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
