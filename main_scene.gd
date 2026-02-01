ac47f61dd6cfc4153691c6fb9432c4259e656c8e
k3mystra <programmer.coleslaw981@slmail.me>
2026-02-01 13:49:02 +0800

    feat: Add boss defeated check

--- main_scene.gd
+++ main_scene.gd
@@ -3,13 +3,20 @@ extends Node2D
 var pause_times
 var target_name
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_cancel"):
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
 		pause_scene()
-			
+
+
 func pause_scene():
 	get_tree().paused = not get_tree().paused
 
+
 func set_target_name(enemy_name):
 	target_name = enemy_name
-	print(target_name)
+
+
+func _on_boss_defeated(_exp_value: int) -> void:
+	print("Boss defeated! You win!")
+	get_tree().paused = true
